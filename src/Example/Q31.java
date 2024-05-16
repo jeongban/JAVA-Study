9b027463ecaaaae9b500e9e323061498d9935860
jeongban <cox82910@naver.com>
2024-05-16 23:23:41 +0900

    example

--- src/Example/Q31.java
+++ src/Example/Q31.java
@@ -7,11 +7,18 @@ class pencil{
 }
 
 class eraser {
-
+    String made = "";
+    void eraserPrint() {
+        System.out.println("지우개를 " + made +" 제품으로 지웁니다.");
+    }
 }
 
 class pen {
-
+    String color = "";
+    String made = "";
+    void penPrint() {
+        System.out.println(made + " 제품의 " + color +"색으로 씁니다.");
+    }
 }
 public class Q31 {
     public static void main(String[] args) {
