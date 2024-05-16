f7ee1d691c32e116546de61ebdfc3ed7e5def5f2
jeongban <cox82910@naver.com>
2024-05-16 23:19:10 +0900

    new example

---
+++ src/Example/Q31.java
@@ -0,0 +1,20 @@
+package Example;
+class pencil{
+    String color = "";
+    void pencilPrint() {
+        System.out.println("쓰는 기능은 " + color + " 연필로 적습니다.");
+    }
+}
+
+class eraser {
+
+}
+
+class pen {
+
+}
+public class Q31 {
+    public static void main(String[] args) {
+
+    }
+}
